18bf315a6b3b0b1bf2a979606ed764da5ccd90d9
Craig Smith <hello@craigiansmith.com.au>
2020-12-14 15:45:22 +1100

    Add question and answer models to Quiz Show
    They share a through table which marks each answer if it is correct.
    Includes some tweaking of the admin UI.

--- quizshow/admin.py
+++ quizshow/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice, QuestionsAndAnswers
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    fields = ['text']
+
+
+class QuestionsAndAnswersInline(admin.TabularInline):
+    model = QuestionsAndAnswers
+    inlines = [ChoiceInline]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['text']
+    inlines = [QuestionsAndAnswersInline]
+
+#    def get_form(self, request, obj=None, change=False, **kwargs):
+#        form = super().get_form(request, obj, change, **kwargs)
+#        form.base_fields['QuestionsAndAnswers'].label = 'Possible answers'
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- quizshow/models.py
+++ quizshow/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+# Choices (one question to several choices, but also choices can appear in multiple questions)
+# 
+class Choice(models.Model):
+    text = models.CharField('a possible answer', max_length=256)
+
+    def __str__(self):
+        return self.text
+
+
+# Questions
+# A question has a one to one relation to a choice as the correct answer
+# but has several other choices as well.
+# It might be desirable to allow short answers as well. This can be done later.
+# A question might have optional media, too.
+# I want some way to track which questions a learner has answered and which they have
+# answered correctly.
+# I also want to provide an aggregate of the ratio of correct to incorrect answers for 
+# each question for a class, a group, or several classes.
+class Question(models.Model):
+    text = models.CharField('a question', max_length=512)
+    choices = models.ManyToManyField(Choice, through='QuestionsAndAnswers', verbose_name='possible answers')
+
+    def __str__(self):
+        return self.text
+
+
+class QuestionsAndAnswers(models.Model):
+    questions = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choices = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.questions}-{self.correct}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['questions', 'choices'], name='question_and_possible_answers')
+        ]
