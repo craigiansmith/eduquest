608837503513cac1ed9d669646ab1ec42f87b79a
Craig Smith <hello@craigiansmith.com.au>
2021-01-03 12:18:23 +1100

    Revert "Add plugins and dependencies"
    This reverts commit e651091edd2f84be0eff673011c8c4c77d6e7030.

--- config/urls.py
+++ config/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,4 +22,4 @@ urlpatterns = [
     path('quizshow/', include('quizshow.urls')),
     path('', include('pages.urls')),
     path('blog/', include('cms.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
